d205ff756e5288693bae423decbbc1d50335d8fe
Derek Haller <dhaller.personal@gmail.com>
2017-09-29 17:49:46 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,14 +5,8 @@ $(document).ready(function(){
 getIt();
 });
 
-function getIt() {
+function getIt () {
   $("p").on("click", function() {
     alert("Hey!");
-  })
-}
-
-function frameIt() {
-  $("img").on("load", function(), {
-    $("img").className = "tasty";
   })
 }
