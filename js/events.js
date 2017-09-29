36bae4959569c2d22a6003f8b5fd048d3d683850
Derek Haller <dhaller.personal@gmail.com>
2017-09-29 18:41:33 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
-
 $(document).ready(function(){
-
-// call functions here
-
+frameIt();
+getIt();
+pressIt();
+submitIt();
 });
+
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    $("img").addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $("#typing").on("keydown", function(e) {
+    if (e.which === 71) {
+      alert("You have pressed G!");
+    }
+  })
+}
+
+function submitIt() {
+  $("form:last").on("submit", function() {
+    alert ("Your form is going to be submitted now.");
+  })
+}
